cd3aadfffd3950152240fef955c6e450eff4fbce
bitterindosisnatama <bitterindosisnatama@gmail.com>
2026-02-11 02:16:24 +0700

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -1,16 +1,13 @@
 const hamburger = document.getElementById("hamburger");
 const navbar = document.getElementById("navbarDynamic");
 
-let isOpen = false;
-
 hamburger.addEventListener("click", () => {
-    if (!isOpen) {
-        navbar.classList.add("scale-in-hor-left");
-        navbar.style.transform = "scaleX(1)";
-        isOpen = true;
-    } else {
-        navbar.classList.remove("scale-in-hor-left");
+    navbar.classList.toggle("active");
+    navbar.classList.toggle("scale-in-hor-left");
+
+    if (navbar.style.transform === "scaleX(1)") {
         navbar.style.transform = "scaleX(0)";
-        isOpen = false;
+    } else {
+        navbar.style.transform = "scaleX(1)";
     }
 });
